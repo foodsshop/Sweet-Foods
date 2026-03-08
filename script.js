1c514a5ecaa9eefbe549aa1e2fa6ef7321dfc3a6
foodsshop <santuk9657@gmail.com>
2026-03-08 20:23:08 +0530

    Update and rename scripts.js to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let delivery = 0
+
+function updateDelivery(){
+
+delivery = parseInt(document.getElementById("delivery").value)
+
+document.getElementById("deliveryCharge").innerText = delivery
+
+update()
+
+}
